e440341a733e3084314dee7fda13b140d9ff589d
EvaHautecler <eva.hautecler@outlook.com>
2024-07-25 15:58:48 +0200

    Updating Game1 class

--- GeometryWars_EvaHautecler/GeometryWars_EvaHautecler/Game1.cs
+++ GeometryWars_EvaHautecler/GeometryWars_EvaHautecler/Game1.cs
@@ -16,8 +16,9 @@ namespace GeometryWars_EvaHautecler
         private GraphicsDeviceManager _graphics;
 
         public SpriteBatch SpriteBatch { get; private set; }
+        private IGameState currentState;
 
-        private SpriteBatch _spriteBatch;
+        /*private SpriteBatch _spriteBatch;
         private Texture2D backgroundTexture;
         private Texture2D spaceshipTexture;
         private Texture2D spaceshipLaserTexture;
@@ -29,7 +30,7 @@ namespace GeometryWars_EvaHautecler
         private float enemy1SpawnCooldown = 2f;
         private float enemy1SpawnTimer;
         private KeyboardReader keyboardReader;
-        private LaserManager laserManager;
+        private LaserManager laserManager;*/
 
         public Game1()
         {
@@ -45,21 +46,23 @@ namespace GeometryWars_EvaHautecler
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            keyboardReader = new KeyboardReader();
+            /*keyboardReader = new KeyboardReader();
             laserManager = new LaserManager();
             spaceship = new Spaceship(spaceshipTexture,spaceshipLaserTexture, keyboardReader);
             random = new Random();
-            enemies = new List<Enemy>();
+            enemies = new List<Enemy>();*/
 
+            ChangeState(new MainMenuState(this));
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            /*_spriteBatch = new SpriteBatch(GraphicsDevice);
             backgroundTexture = Content.Load<Texture2D>("Background");
             spaceshipTexture = Content.Load<Texture2D>("Spaceship");
             spaceshipLaserTexture = Content.Load<Texture2D>("Charge");
-            enemy1Texture = Content.Load<Texture2D>("Enemy1");
+            enemy1Texture = Content.Load<Texture2D>("Enemy1");*/
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
         }
@@ -71,7 +74,7 @@ namespace GeometryWars_EvaHautecler
 
             // TODO: Add your update logic here
 
-            spaceship.Update(gameTime);
+            /*spaceship.Update(gameTime);
             laserManager.Update(gameTime);
             enemy1SpawnTimer -= (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (enemy1SpawnTimer <= 0)
@@ -92,13 +95,15 @@ namespace GeometryWars_EvaHautecler
                         break;
                     }
                 }
-            }
+            }*/
+
+            currentState?.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            _spriteBatch.Begin();
+            /*_spriteBatch.Begin();
             _spriteBatch.Draw(backgroundTexture, new Rectangle(0, 0, 2000, 988), Color.White);
             spaceship.Draw(_spriteBatch);
             laserManager.Draw(_spriteBatch);
@@ -107,12 +112,20 @@ namespace GeometryWars_EvaHautecler
             {
                 enemy.Draw(_spriteBatch);
             }
-            _spriteBatch.End();
+            _spriteBatch.End();*/
+
+            currentState?.Draw(gameTime);
 
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
 
+        public void ChangeState(IGameState newState)
+        {
+            currentState?.Exit();
+            currentState = newState;
+            currentState.Enter();
+        }
     }
 }
